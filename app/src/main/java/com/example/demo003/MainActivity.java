8332d89cc999e1a11055a39408abae1214a420f6
LFQ0120 <2352263649@qq.com>
2020-09-29 20:56:33 +0800

    第一次作业

---
+++ app/src/main/java/com/example/demo003/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.demo003;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.demo003.utils.DisplayUtil;
+import com.example.demo003.utils.Utils;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView text_screen;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text_screen = findViewById(R.id.text1);
+
+        int dip_10 = Utils.dip2px(this, 10L);
+        text_screen.setPadding(dip_10, dip_10, dip_10, dip_10);
+        text_screen.setBackgroundColor(0xff00ffff);
+        text_screen.setTextColor(0xff333333);
+        showScreenInfo();
+
+        TextView textView3 = findViewById(R.id.text1);
+        textView3.setTextColor(getResources().getColor(R.color.colorAccent));
+    }
+
+    private void showScreenInfo() {
+        int width = DisplayUtil.getScreenWidth(this);
+        int height = DisplayUtil.getScreenHeight(this);
+        float sensity = DisplayUtil.getScreenDensity(this);
+        String info = String.format("当前屏幕的宽度是%dpx，高度是%dpx，像素密度是%f", width, height, sensity);
+        text_screen.setText(info);
+    }
+}
